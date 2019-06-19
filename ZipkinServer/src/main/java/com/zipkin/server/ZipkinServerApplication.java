f145e1e4ce94be6a261a567e50d5cfb38bda164b
felipe.palacios <felipe.palacios@tata.com.uy>
2019-06-19 14:52:37 -0300

    se termina configuracion del server

--- ZipkinServer/src/main/java/com/zipkin/server/ZipkinServerApplication.java
+++ ZipkinServer/src/main/java/com/zipkin/server/ZipkinServerApplication.java
@@ -2,14 +2,12 @@ package com.zipkin.server;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 import zipkin2.server.internal.EnableZipkinServer;
 
 
 @SpringBootApplication
 @EnableZipkinServer
-@EnableEurekaClient
 public class ZipkinServerApplication {
 
 	public static void main(String[] args) {
